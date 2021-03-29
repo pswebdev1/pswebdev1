e1b9c29ec30951c564400281a959c173b70818bb
pswebdev1 <81532427+pswebdev1@users.noreply.github.com>
2021-03-29 11:31:46 +0200

    Add files via upload

---
+++ incs/connect.php
@@ -0,0 +1,15 @@
+<?
+	/*$servername = "localhost";
+	$username = "jamal1234";
+	$password = "cudgunofcu60";
+	$mydb = my_jamal1234;
+	*/
+	
+	$con = mysqli_connect("localhost", "jamal1234", "cudgunofcu60", "my_jamal1234");
+	if (!$con) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+?>
